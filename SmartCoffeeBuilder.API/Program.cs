c11ec293656726d195ae824de8e603324307748c
LuanNguyen-Gog <luannguyen75402@gmail.com>
2026-06-09 21:31:57 +0700

    Connect from DB and create models

--- SmartCoffeeBuilder.API/Program.cs
+++ SmartCoffeeBuilder.API/Program.cs
@@ -1,10 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using SmartCoffeeBuilder.Repository.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<SmartCafeBuilderContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
